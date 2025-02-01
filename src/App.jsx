8ec8f35fd65b5d63c55e7199de4ec385cbe66c15
Rofix <wourkout123@gmail.com>
2025-02-02 00:30:43 +0100

    Setup Project Requiements:  1- install react by vite  2- install tailwindcss v3  3- install daisyui  4- add daisyui in plugins  5- use dracula theme

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <p className="text-red-500">
+        hey test hey test hey test hey test hey test hey test{" "}
+      </p>
+      <div className="text-3xl text-red-600 text-center mt-10">Hello Devs</div>
+    </>
+  );
+};
+
+export default App;
