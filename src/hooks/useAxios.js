1dd342c15b0774c255de34decac232422ba4e54e
Rofix <wourkout123@gmail.com>
2025-02-03 23:49:16 +0100

    - Create custom hook => useAxios (for fetch data) - pass three arguments (url, method, options) - use useCallback hook and useMemo and useEffect for optomize performance.

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,54 @@
+import { useState , useEffect, useCallback, useMemo} from 'react'
+import axios from '../api' 
+import {isAxiosError} from 'axios'
+const useAxios = (url, method = 'GET', options = {}) => {
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null);
+
+    const stableOptions = useMemo(() => JSON.stringify(options), [options]);
+
+    const fetchData = useCallback(async () => {
+        setIsLoading(true)
+        setError(null)
+        try {
+            const {data} = await axios({
+                url,
+                method,
+               ...options
+            })
+            console.log(
+                '%cRESPONSE',
+                'color: green; font-family: sans-serif;font-size:30px'
+              );
+              console.log(data);
+              
+              console.log(
+                '%cEND RESPONSE',
+                'color: green; font-family: sans-serif;font-size:30px'
+              );
+            
+            setData(data)
+        }
+        catch(err) {
+            let error = null;
+
+            if(isAxiosError(err)) {
+                console.error('AXIOS ERROR', error.response.data.message)
+                error = err.response.data.message
+            } else {
+                console.error('API ERROR', err)
+                error = err.message|| 'An error occurred while fetching data.'
+            }
+            setError(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }, [url, method, stableOptions])
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
+}
+
+export default useAxios
